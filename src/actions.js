767df94c89f8e3d1e40cbfccfb5ccfb24333299d
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-04 11:48:11 +0530

    feat: added functionality to add new task and display it on the view

---
+++ src/actions.js
@@ -0,0 +1,5 @@
+export const ADD_TASK="task/added"
+export const REMOVE_TASK="task/remove"
+export const TOGGLE_TASK ="task/toggled"
+export const CALCULATE_TOTAL_TASKS="task/calculateTotalTasks"
+export const addTask =(task)=>({type:ADD_TASK,payload:task})
--- src/main.js
+++ src/main.js
@@ -1,2 +1,33 @@
+import { createStore } from 'redux'
 import './style.css'
-
+import taskReducer from './taskReducer'
+import { addTask } from './actions'
+const title=document.getElementById("title")
+const description=document.getElementById("description")
+const addNewTask=document.getElementById("addNewTask")
+const removeTaskId=document.getElementById("removeTaskId")
+const displayList=document.getElementById("displayList")
+const displayTotal=document.getElementById("displayTotal")
+const removeTask=document.getElementById("removeTask")
+const store=createStore(taskReducer)
+const addNewTaskEventHandler=()=>{
+  const state=store.getState()
+  const newTask={
+    id:state.tasks.length+1,
+    title:title.value,
+    description:description.value,
+    status:false
+  }
+  store.dispatch(addTask(newTask))
+}
+addNewTask.addEventListener("click",addNewTaskEventHandler)
+const renderTaskList=()=>{
+  const state=store.getState()
+ displayList.innerHTML =state.tasks.map(task=>
+  `<li><input type="checkbox"/> ${task.id}. ${task.title}: ${task.description}</li>`
+  ).join("")
+}
+renderTaskList()
+store.subscribe(()=>{
+  renderTaskList()
+})
